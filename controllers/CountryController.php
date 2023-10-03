d706127a7d90ecf61fb55d1a06c832a13202b5a5
Liza Pronina <proninaelizaveta2006@gmail.com>
2023-10-03 14:54:33 +0300

    new controller and country pages

---
+++ controllers/CountryController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\ContactForm;
+use app\models\MailForm;
+use Yii;
+use yii\web\Controller;
+use yii\data\Pagination;
+use app\models\Country;
+
+class CountryController extends Controller
+{
+    public function actionIndex()
+    {
+        $query = Country::find();
+
+        $pagination = new Pagination([
+            'defaultPageSize' => 5,
+            'totalCount' => $query->count(),
+        ]);
+
+        $countries = $query->orderBy('name')
+            ->offset($pagination->offset)
+            ->limit($pagination->limit)
+            ->all();
+
+        return $this->render('index', [
+            'countries' => $countries,
+            'pagination' => $pagination,
+        ]);
+    }
+    public function actionForm()
+    {
+        $model = new MailForm();
+        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
+            Yii::$app->session->setFlash('contactFormSubmitted');
+
+            return $this->refresh();
+        }
+        return $this->render('form', [
+            'model' => $model,
+        ]);
+    }
+}
